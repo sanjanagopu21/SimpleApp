4341544f2c47a24279a5d0fc18e426692c6b4425
Sanjana Reddy <sanjanadev9989@gmail.com>
2020-06-18 22:01:58 +0000

    added readme.md and updated hello.java with comments

--- Hello.java
+++ Hello.java
@@ -1,3 +1,5 @@
+// This program will print Hello World
+
 public class Hello {
 
     public static void main(String[] args) {
---
+++ README.md
@@ -0,0 +1,6 @@
+pplication Description: this is a simple web app.
+
+Dev:
+
+Dev A
+Dev B
